38bc05e974d6d9c76f575b5161ef20756ee09b17
manpreet singh <manpreet_jsb@hotmail.com>
2021-07-17 00:21:23 +0200

    updating prettier and linting

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+
+const App: React.FC = () => (
+  <div className='App'>
+    <header className='App-header'>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </header>
+  </div>
+)
+
+export default App
