7cca1bd86dad4c251b685206203bc77c046dadac
manpreet singh <manpreet_jsb@hotmail.com>
2021-07-18 11:29:12 +0200

    added image in bg

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
-import Box from '@material-ui/core/Box'
+import { Box, Container } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 
-const Home: React.FC = () => <Box>Home</Box>
+const Home: React.FC = () => (
+  <>
+    <Container>
+      <Grid container justify='space-between'>
+        <Box m={5}>#Portfolio</Box>
+        <Box m={5}>
+          <ul>
+            <li>Projects</li>
+            <li>About me</li>
+            <li>Technologies</li>
+          </ul>
+        </Box>
+        <Box m={5}>
+          <GitHubIcon />
+          <LinkedInIcon />
+        </Box>
+      </Grid>
+    </Container>
+  </>
+)
 
 export default Home
