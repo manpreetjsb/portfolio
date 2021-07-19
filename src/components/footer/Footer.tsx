ed33d85deda43abf057e545fc83bdbdd2c16ee5d
manpreet singh <manpreet_jsb@hotmail.com>
2021-07-19 21:42:21 +0200

    change font size

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import SocialLinks from '../socialLinks/SocialLinks'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
+import SocialLinks from '../socialLinks/SocialLinks'
 
 const Footer = () => (
   <Grid container justify='space-between'>
