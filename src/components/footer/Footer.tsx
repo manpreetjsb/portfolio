7a6dd74655f32fa4669d078ddd72effe2245f6a1
manpreet singh <manpreet_jsb@hotmail.com>
2021-07-19 21:35:53 +0200

    made font size smaller

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,15 @@
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 import React from 'react'
 import SocialLinks from '../socialLinks/SocialLinks'
 
 const Footer = () => (
   <Grid container justify='space-between'>
-    <Box m={5} color='primary'>
-      © Manpreet Singh 2021
+    <Box m={5}>
+      <Typography variant='body1' component='h5'>
+        © Manpreet Singh 2021
+      </Typography>
     </Box>
     <Box m={5} display='flex'>
       <SocialLinks />
