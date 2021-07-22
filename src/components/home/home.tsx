66b3ff21a13d2189e7cd9ed41991aad85be3f75b
manpreet singh <manpreet_jsb@hotmail.com>
2021-07-22 20:07:40 +0200

    added about-us

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Box, Container } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid'
+import Divider from '@material-ui/core/Divider'
 import SocialLinks from '../socialLinks/SocialLinks'
 import Footer from '../footer/Footer'
 import Technologies from '../technologies/Technologies'
+import AboutUs from '../about-us/About-us'
 
 const Home: React.FC = () => (
   <>
@@ -22,6 +24,10 @@ const Home: React.FC = () => (
       <Box m={10} component='h2'>
         Hi, My name is Manpreet Singh, I am Frontend Developer.
       </Box>
+      <Box>
+        <AboutUs />
+      </Box>
+      <Divider />
       <Box>
         <Technologies />
       </Box>
